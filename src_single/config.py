85d4ece789dd916b7276912a105c602ef4c7512c
wchen342 <feiyu2817@gmail.com>
2018-03-08 16:50:05 -0500

    Add comments

--- src_single/config.py
+++ src_single/config.py
@@ -3,12 +3,12 @@
 
 class Config(object):
     # global options
-    data_format = 'NCHW'
-    sn = False
-    proj_d = False
-    wgan = False
+    data_format = 'NCHW'    # DO NOT CHANGE THIS
     SPECTRAL_NORM_UPDATE_OPS = "spectral_norm_update_ops"
-    pre_calculated_dist_map = True
+    sn = False   # Whether uses Spectral Normalization(https://arxiv.org/abs/1802.05957)
+    proj_d = False    # Whether uses projection discriminator(https://arxiv.org/abs/1802.05637)
+    wgan = False    # WGAN or DRAGAN(only effective if sn is False)
+    pre_calculated_dist_map = True    # Whether calculate distance maps on the fly
 
     @staticmethod
     def set_from_dict(d):
